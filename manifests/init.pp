3d57ac966a5481ab9f9f5207681581e87d0c9fce
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-18 15:01:11 +0800

    This should do it...

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,14 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install PDFpen into /Applications.
+#
+# Examples
+#
+#   include pdfpen
+class pdfpen (
+  $version = '6.3.2',
+){
+  package { 'PDFpen':
+    ensure   => 'installed',
+    provider => 'compressed_app',
+    source   => "http://cdn.smilesoftware.com/PDFpen_${version}.zip",
+  }
 }
--- spec/classes/pdfpen_spec.rb
+++ spec/classes/pdfpen_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 # Rename this file to classname_spec.rb
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+describe 'pdfpen' do
   it do
-    should contain_anchor('Hello_World')
+    should contain_package('PDFpen').with({
+      :ensure   => 'installed',
+      :provider => 'compressed_app',
+    })
   end
 end
