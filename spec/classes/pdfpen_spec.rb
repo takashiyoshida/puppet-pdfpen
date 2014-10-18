ee53e09ad26d5058de20f5172e1e5114e6aedf6f
Takashi Yoshida <takashi@takashiyoshida.org>
2014-10-18 15:08:06 +0800

    Fix missing class

--- spec/classes/pdfpen_spec.rb
+++ spec/classes/pdfpen_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
-# Rename this file to classname_spec.rb
-# Check other boxen modules for examples
-# or read http://rspec-puppet.com/tutorial/
+
 describe 'pdfpen' do
   it do
     should contain_package('PDFpen').with({
       :ensure   => 'installed',
-      :provider => 'compressed_app',
+      :provider => 'compressed_app'
     })
   end
 end
